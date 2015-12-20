76d72103887f197f01d0e655da6ae5b079418a71
Nikki Tan <nikkigoldcal@gmail.com>
2015-12-19 17:06:27 -0800

    Initial commit.

---
+++ src/main/java/com/mmtc/exam/MmtcAuthSuccessHandler.java
@@ -0,0 +1,25 @@
+package com.mmtc.exam;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class MmtcAuthSuccessHandler implements AuthenticationSuccessHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1, Authentication arg2)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		logger.info("onAuthSuccess!!");
+	}
+
+}
