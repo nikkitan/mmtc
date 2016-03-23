0bb0d0eb763374c6c2fe988ee29bd11e246d5919
Nikki Tan <nikki@TanNikkis-MacBook-Pro.local>
2016-03-23 09:41:43 -0700

    Put on copyright header.

--- src/main/java/com/mmtc/exam/dao/TestSuite.java
+++ src/main/java/com/mmtc/exam/dao/TestSuite.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 Mendez Master Training Center
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 package com.mmtc.exam.dao;
 
 public class TestSuite {
